d6e5923c4b59c075ab1967fa6555f1cfe9bed197
Blaine <blaine.d.oneill@gmail.com>
2022-07-20 16:04:23 -0400

    fix(app): add missing dependencies

--- app/components/post-body.tsx
+++ app/components/post-body.tsx
@@ -1,9 +1,8 @@
-import markdownStyles from './markdown-styles.module.css'
 import { PortableText } from '@portabletext/react'
 
 export default function PostBody({ content }) {
   return (
-    <div className={`max-w-2xl mx-auto ${markdownStyles.markdown}`}>
+    <div className={`max-w-2xl mx-auto`}>
       <PortableText value={content} />
     </div>
   )
