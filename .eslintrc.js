e9d45e9cecfa8a4b0a71fa57d37e1d8fa459e825
Blaine <blaine.d.oneill@gmail.com>
2022-07-19 23:58:20 -0400

    chore(repo): yarn eslint babel husky config

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@next/next/recommended',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'import/no-cycle': 'error',
+    'import/no-unresolved': [2, { ignore: ['^(all|part):'] }],
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
