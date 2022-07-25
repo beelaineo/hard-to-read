7e1b01fb4ade6ad6d236cc7d073ab3aaa0a5c23c
Blaine <blaine.d.oneill@gmail.com>
2022-07-24 23:20:26 -0400

    fix(app): date error

--- app/components/post-header.tsx
+++ app/components/post-header.tsx
@@ -6,9 +6,7 @@ export default function PostHeader({ title, date }) {
     <>
       <PostTitle>{title}</PostTitle>
       <div className="max-w-2xl mx-auto">
-        <div className="mb-6 text-lg">
-          <Date dateString={date} />
-        </div>
+        <div className="mb-6 text-lg">{date}</div>
       </div>
     </>
   )
