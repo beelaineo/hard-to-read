88dd7b9ab008e7d0c24adb9bb281ae13b11118c3
Blaine <blaine.d.oneill@gmail.com>
2022-07-24 21:26:45 -0400

    fix(app): add dynamic Title in header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,16 @@
 import Link from 'next/link'
 import { x } from '@xstyled/styled-components'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+  const router = useRouter()
+  const path = router.pathname
+
+  const title =
+    path == '/about'
+      ? 'Hard'
+      : router.pathname.slice(1).replace(/^\w/, (c) => c.toUpperCase())
+
   return (
     <x.header position={'fixed'} left={0} right={0} top={0} m={4}>
       <x.h1
@@ -12,7 +21,7 @@ export default function Header() {
       >
         <Link href="/">
           <x.a gridColumn={'span 3'} textDecoration={{ hover: 'underline' }}>
-            Hard
+            {title}
           </x.a>
         </Link>
         <Link href="/">
