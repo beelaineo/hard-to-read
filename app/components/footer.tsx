e4f3cf8731ba49ca5c9e238c13fbc10e08c83e3a
Blaine <blaine.d.oneill@gmail.com>
2022-07-24 21:05:04 -0400

    fix(app): active nav links, press page separate, conditional length on quack

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,24 +1,41 @@
 import * as React from 'react'
+import { useRouter } from 'next/router'
 import Image from 'next/image'
-import Link from 'next/link'
+import NavLink from 'next/link'
 import styled, { x, css } from '@xstyled/styled-components'
 import { useSiteData } from '../providers/SiteDataProvider'
 import { useModal } from '../providers/ModalProvider'
 import { modalize } from '../utils'
 const { useState, useEffect } = React
 
+interface WithActive {
+  active: boolean
+}
+
+const StyledLink = styled.a<WithActive>`
+  ${({ active }) => css`
+    text-decoration: ${active ? 'underline' : 'none'};
+    :hover {
+      text-decoration: underline;
+    }
+  `}
+`
+
 export default function Footer() {
   const siteData = useSiteData()
   const socials = siteData?.siteSettings?.socials ?? []
-
-  const { resetModals } = useModal()
+  const router = useRouter()
+  const { modals, resetModals } = useModal()
+  const pathName = router.pathname
 
   const [pageFX, setPageFX] = useState<HTMLAudioElement | null>(null)
   useEffect(() => setPageFX(new Audio('/page.mp3')), [])
 
   const handleQuack = () => {
-    pageFX?.play()
-    resetModals()
+    if (modals.length > 0) {
+      pageFX?.play()
+      resetModals()
+    }
   }
 
   return (
@@ -36,34 +53,36 @@ export default function Footer() {
     >
       <x.section gridColumn={'span 3'} p={4} bg={'gray-200'}>
         <x.nav display={'flex'} flexDirection={'column'}>
-          <Link href="/blog">
-            <x.a textDecoration={{ hover: 'underline' }}>Blog</x.a>
-          </Link>
-          <Link href="/about">
-            <x.a textDecoration={{ hover: 'underline' }}>About</x.a>
-          </Link>
-          <Link href="/press">
-            <x.a textDecoration={{ hover: 'underline' }}>Press</x.a>
-          </Link>
+          <NavLink href="/blog">
+            <StyledLink active={pathName === '/blog'}>Blog</StyledLink>
+          </NavLink>
+          <NavLink href="/about">
+            <StyledLink active={pathName === '/about'}>About</StyledLink>
+          </NavLink>
+          <NavLink href="/press">
+            <StyledLink active={pathName === '/press'}>Press</StyledLink>
+          </NavLink>
         </x.nav>
       </x.section>
       <x.section gridColumn={'span 4'} p={4} bg={'white'}>
         <x.nav display={'flex'} flexDirection={'column'}>
-          <Link href="/events">
-            <x.a textDecoration={{ hover: 'underline' }}>Events</x.a>
-          </Link>
-          <Link href="/exhibitions">
-            <x.a textDecoration={{ hover: 'underline' }}>Exhibitions</x.a>
-          </Link>
-          <Link href="/people">
-            <x.a textDecoration={{ hover: 'underline' }}>People</x.a>
-          </Link>
-          <Link href="/themes">
-            <x.a textDecoration={{ hover: 'underline' }}>Themes</x.a>
-          </Link>
-          <Link href="/partners">
-            <x.a textDecoration={{ hover: 'underline' }}>Partners</x.a>
-          </Link>
+          <NavLink href="/events">
+            <StyledLink active={pathName === '/events'}>Events</StyledLink>
+          </NavLink>
+          <NavLink href="/exhibitions">
+            <StyledLink active={pathName === '/exhibitions'}>
+              Exhibitions
+            </StyledLink>
+          </NavLink>
+          <NavLink href="/people">
+            <StyledLink active={pathName === '/people'}>People</StyledLink>
+          </NavLink>
+          <NavLink href="/themes">
+            <StyledLink active={pathName === '/themes'}>Themes</StyledLink>
+          </NavLink>
+          <NavLink href="/partners">
+            <StyledLink active={pathName === '/partners'}>Partners</StyledLink>
+          </NavLink>
         </x.nav>
       </x.section>
       <x.section gridColumn={'span 3'} p={4} bg={'white'} position={'relative'}>
