28ad463039c3a9a653afd6138e4c7afe1063d0bb
Blaine <blaine.d.oneill@gmail.com>
2022-09-14 12:19:45 -0400

    add gtag (GA4 tag)

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,10 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Google tag (gtag.js - GA4) */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-8FHF2D3337"
+          />
+          <Script
+            id="gtag"
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8FHF2D3337')`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
